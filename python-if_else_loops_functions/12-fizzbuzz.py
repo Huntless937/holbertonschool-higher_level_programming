ce5a3855ff75371c0e3b02686529616736521b8d
Huntless937 <fidailveliyev09@gmail.com>
2026-03-25 17:31:47 +0000

    12th

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-import sys
-
-
-def factorial(n):
-    result = 1
-    while n > 1:
-        result *= n
-        n -= 1
-    return result
-
-
-if len(sys.argv) > 1:
-    f = factorial(int(sys.argv[1]))
-    print(f)
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print("{}".format(i), end=" ")
