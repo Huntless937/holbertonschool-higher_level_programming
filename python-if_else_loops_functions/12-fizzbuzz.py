93ceecc2e9bb0df268d8a555e0515cad765cfff7
Huntless937 <fidailveliyev09@gmail.com>
2026-03-25 16:49:55 +0000

    12th

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 import sys
 
+
 def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # This is the missing line!
+        n -= 1
     return result
 
-# It's good practice to check if an argument was actually provided
+
 if len(sys.argv) > 1:
     f = factorial(int(sys.argv[1]))
     print(f)
