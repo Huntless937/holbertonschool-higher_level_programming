bcc0ad5e89dfb80b5585a0d2e1e5b454ffadecfb
Huntless937 <fidailveliyev09@gmail.com>
2026-03-26 17:06:32 +0000

    4th

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that contains the inherits_from function."""
+
+
+def inherits_from(obj, a_class):
+    """Check if an object is an instance of a class that inherited from a class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match against.
+
+    Returns:
+        bool: True if obj inherited (directly or indirectly) from a_class,
+              but is not an instance of a_class itself. Otherwise False.
+    """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
