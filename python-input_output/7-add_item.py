f71ef6f9506c79b6f3e41ed4182e6d567f6ee0b1
Huntless937 <fidailveliyev09@gmail.com>
2026-03-28 13:47:58 +0000

    7th

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Script that adds all arguments to a Python list, and saves them to a file."""
+"""
+Script that adds all arguments to a Python list,
+and saves them to a file.
+"""
 import sys
 
 
